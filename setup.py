4a7fd0a84dedfe201dd3053c2d092dbd5fdb7b02
Bas Wijnen <wijnen@debian.org>
2025-06-10 00:39:30 +0200

    release 1.0

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ module = Extension('lua',
 	extra_compile_args = ['-I/usr/include/lua5.4'],
 )
 
-setup(name = 'lua',
-	version = '0.6',
+setup(name = 'lua-wrapper',
+	version = '1.0',
 	description = 'Allow Lua and Python scripts to work together',
 	ext_modules = [module],
 )
